25785dba711c16661aed0ff1976e6a2f5163a52e
Macbook <galvaopp@gmail.com>
2020-11-08 23:22:18 -0300

    Feature: Configuration base PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado - Bolerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="minifest" href="manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJsand Style Component"
